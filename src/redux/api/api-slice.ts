0b797436f8aae250bb514fa20db2b7362493f0d4
alifsakib <alifsakib@gmail.com>
2023-07-17 23:16:46 +0600

    added backend url

--- src/redux/api/api-slice.ts
+++ src/redux/api/api-slice.ts
@@ -2,6 +2,8 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const api = createApi({
   reducerPath: "api",
-  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:5000/api/v1" }),
+  baseQuery: fetchBaseQuery({
+    baseUrl: "https://book-lab-server-alifsakib.vercel.app/api/v1",
+  }),
   endpoints: () => ({}),
 });
